8f42f8448f36f2905dfe827c8d224d63edac3919
Haekal Rizky Yulianto <haekal.yulianto@outlook.com>
2022-08-26 14:18:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,23 +151,23 @@ if selected == "Level Penempatan Dana":
     nama_bank = st.sidebar.selectbox('Nama Bank : ',(st.session_state['df2']['Nama Bank']))
     bank_level = st.sidebar.number_input('Level : ', value=1, step=1)
         
-    # Menjalankan Analisis Interconnectedness Bank
+    # Menjalankan Level Penempatan Dana
     if st.sidebar.button('Run'):
-        st.header("Hasil Analisis Interconnectedness Bank")
+        st.header("Hasil Analisis Level Penempatan Dana")
 
         # Bank Level
-        st.success('Hasil Analisis Interconnectedness Bank : ' + nama_bank + ' Level : ' + str(bank_level) + ' (Periode : ' + st.session_state['periode'] + ')')
+        st.success('Hasil Analisis Level Penempatan Dana Bank : ' + nama_bank + ' Level : ' + str(bank_level) + ' (Periode : ' + st.session_state['periode'] + ')')
         df6 = util.view_data_from_bank_level(st.session_state['df5'], nama_bank, bank_level, st.session_state['df2'])
         file = open('/tmp/graph_bank_level.html', 'r', encoding='utf-8')
         source = file.read()
         components.html(source, height = 500)
 
         # Dataframe
-        st.success('Tabel Analisis Interconnectedness Bank : ' + nama_bank + ' Level : ' + str(bank_level) + ' (Periode : ' + st.session_state['periode'] + ')')
+        st.success('Tabel Analisis Level Penempatan Dana : ' + nama_bank + ' Level : ' + str(bank_level) + ' (Periode : ' + st.session_state['periode'] + ')')
         st.write(df6)
         
         # Graph All
-        st.success('Hasil Analisis Interconnectedness Keseluruhan')
+        st.success('Hasil Analisis Penempatan Dana Keseluruhan')
         # Check graph all exist
         graph_all_file = Path("/tmp/graph_all.html")
         if not graph_all_file.is_file():
